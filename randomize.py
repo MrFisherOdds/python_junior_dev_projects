b1ffa622c0fb6a26b4a21aad207f6dcbe109364e
Max <maxar47@ya.ru>
2022-06-07 11:01:39 +0300

    Adding file

--- randomize.py
+++ randomize.py
@@ -1,38 +1,36 @@
 from random import *
-
-n = randrange(1, 101)
-
-def digit_guess(num):  # угадываем число
-    while num != n:
-        if num > n:
-            num = int(input('Ваше число больше загаданного, попробуйте еще разок: '))
-        elif num < n:
-            num = int(input('Ваше число меньше загаданного, попробуйте еще разок: '))
-
-
-def is_valid(num):  # проверяем вадидность введенного числа
-    if 0 < num < 101:
-        return True
-    else:
-        return False
-
 print()
 print('Добро пожаловать в числовую угадайку!')
-print()
-
-s = int(input('Пожалуйста, введите число от 1 до 100: '))
-
-while not is_valid(s):
-    s = int(input('А может быть все-таки введем целое число от 1 до 100?'))
-
-digit_guess(s)
-print()
-print('Вы угадали, поздравляем!')
-print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+while True:
+    n = randrange(1, 101)
+    try_counter = 0
 
 
+    def digit_guess(num):  # угадываем число
+        counter = 1
+        while num != n:
+            if num > n:
+                num = int(input('Ваше число больше загаданного, попробуйте еще разок: '))
+            elif num < n:
+                num = int(input('Ваше число меньше загаданного, попробуйте еще разок: '))
+            counter += 1
+        global try_counter
+        try_counter = counter
 
 
+    def is_valid(num):  # проверяем вадидность введенного числа
+        if 0 < num < 101:
+            return True
+        else:
+            return False
 
 
+    s = int(input('Пожалуйста, введите число от 1 до 100: '))
 
+    while not is_valid(s):
+        s = int(input('А может быть все-таки введем целое число от 1 до 100? : '))
+
+    digit_guess(s)
+    print()
+    print('Вы угадали число с', try_counter , 'попыток, поздравляем!')
+    print('Спасибо, что играли в числовую угадайку. Введите новое число и сыграйте снова!')
