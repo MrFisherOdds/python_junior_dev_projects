a21b42de381d7312fcccdcdb9c36cda50a7e946c
Max <maxar47@ya.ru>
2022-06-09 12:07:55 +0300

    Adding file

--- Ceasar's_code.py
+++ Ceasar's_code.py
@@ -3,15 +3,13 @@ def shifrovanie():  # Функция шифра
     yazyk = input('Выберите используемый язык (ru / en) : ').upper()
     shag_sdviga = int(input('Выберите длину шага (враво) : '))
     ishodni_text = input('Введите текст для шифрования : ')
-    ishodni_text_spisok = list(ishodni_text)
     russkie_bukvy_mal = 'абвгдежзийклмнопрстуфхцчшщъыьэюяабвгдежзийклмнопрстуфхцчшщъыьэюя'
     russkie_bukvy_bol = 'АБВГДЕЖЗИЙКЛМНОПРСТУФЧЦЧШЩЪЫЬЭЮЯАБВГДЕЖЗИЙКЛМНОПРСТУФЧЦЧШЩЪЫЬЭЮЯ'
     angl_bukvy_mal = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
     angl_bukvy_bol = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
     konechny_spisok = []
-    konechny_text = ''
 
-    if napravlenie == 'CODE' and yazyk == 'EN':
+    if napravlenie == 'CODE' and yazyk == 'EN':  # Этот базац и абзац ниже на случай кодирования
         for i in range(len(ishodni_text)):
             if ishodni_text[i] in angl_bukvy_bol:
                 position = int(angl_bukvy_bol.find(ishodni_text[i]))
@@ -36,6 +34,30 @@ def shifrovanie():  # Функция шифра
         konechny_text = ''.join(konechny_spisok)
         return konechny_text
 
+    if napravlenie == 'DECODE' and yazyk == 'EN':  # Этот базац и абзац ниже на случай ДЕкодирования
+        for i in range(len(ishodni_text)):
+            if ishodni_text[i] in angl_bukvy_bol:
+                position = int(angl_bukvy_bol.rfind(ishodni_text[i]))
+                konechny_spisok.extend(angl_bukvy_bol[position - shag_sdviga % 26])
+            elif ishodni_text[i] in angl_bukvy_mal:
+                position = int(angl_bukvy_mal.rfind(ishodni_text[i]))
+                konechny_spisok.extend(angl_bukvy_mal[position - shag_sdviga % 26])
+            else:
+                konechny_spisok.extend(ishodni_text[i])
 
+        konechny_text = ''.join(konechny_spisok)
+        return konechny_text
+    elif napravlenie == 'DECODE' and yazyk == 'RU':
+        for i in range(len(ishodni_text)):
+            if ishodni_text[i] in russkie_bukvy_bol:
+                position = int(russkie_bukvy_bol.rfind(ishodni_text[i]))
+                konechny_spisok.extend(russkie_bukvy_bol[position - shag_sdviga % 32])
+            elif ishodni_text[i] in russkie_bukvy_mal:
+                position = int(russkie_bukvy_mal.rfind(ishodni_text[i]))
+                konechny_spisok.extend(russkie_bukvy_mal[position - shag_sdviga % 32])
+            else:
+                konechny_spisok.extend(ishodni_text[i])
+        konechny_text = ''.join(konechny_spisok)
+        return konechny_text
 
 print(shifrovanie())
